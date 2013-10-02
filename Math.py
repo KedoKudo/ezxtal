410db85146e5670ea7277bbc4868a440eb884dcf
Chen <chenzhang8722@gmail.com>
2013-10-02 09:51:57 -0400

    added multi-dimension vector class

--- Math.py
+++ Math.py
@@ -9,6 +9,56 @@ __author__ = "KenZ"
 import numpy as np
 
 
+class Vector:
+    """Multidimensinal vector class"""
+    def __init__(self, d):
+        """Initialize with d-dimensional vector of zeros"""
+        self._coords = [0] * d
+
+    def __len__(self):
+        """Return the dimension of the vector"""
+        return len(self._coords)
+
+    def __getattr__(self, item):
+        """Return item_th coordinate of the vector"""
+        return self._coords[item]
+
+    def __setitem__(self, key, value):
+        """Set key_th coordinate with the given value"""
+        self._coords[key] = value
+
+    def __add__(self, other):
+        """Return the sum of two vectors"""
+        if len(self) != len(other):
+            raise ValueError("dimension must agree")
+        result = Vector(len(self))
+        for i in range(len(self)):
+            result[i] = self[i] + other[i]
+        return result
+
+    def __eq__(self, other):
+        """Return true if two vector are the same"""
+        if len(self) != len(other):
+            raise ValueError("dimension must agree")
+        return self._coords == other.__coords
+
+    def __ne__(self, other):
+        """Return True if vector differs from the other"""
+        return not self == other
+
+    def __str__(self):
+        """Produce a string representation of vector"""
+        return "<" + str(self._coords)[1:-1] + ">"  # remove the '[' ']' in the str(list)
+
+    def vector_length(self):
+        """Return the length of a vector"""
+        vector_length = 0
+        for i in range(len(self)):
+            vector_length += self._coords[i] ** 2
+        vector_length = np.sqrt(vector_length)
+        return vector_length
+
+
 def find_angle(sin_val, cos_val):
         """
         find angle in radians based on value of sine and cosine
