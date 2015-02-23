03f28684b4b3168178bc93de5ecccb4c7206834c
Chen <chenzhang8722@gmail.com>
2015-02-23 10:17:39 -0500

    #! fixed

--- unitTest.py
+++ unitTest.py
@@ -3,6 +3,7 @@
 ##
 # Unit test using PET
 #   url: http://docs.python.org/2/library/unittest.html
+# need to work on this later
 
 
 import unittest
@@ -13,7 +14,9 @@ from ezxtal.Orientation import Quaternion
 
 class TestEulerAngle(unittest.TestCase):
     """ Test EulerAngle class """
-    print "Testing Euler Angle"
+
+    def setUp(self):
+        self.euler_list = [[0, 0, 0], [90, 0, 0], [0, 90, 0], [20, 80, 100]]
 
 
 class TestRoationMaitrx(unittest.TestCase):
