0e629cc6ec717ed422218a74a900822eea1a4b3b
Chen <chenzhang8722@gmail.com>
2015-03-23 14:36:52 -0400

    add support for partial clustering using sort_range option

--- Data.py
+++ Data.py
@@ -13,20 +13,23 @@ class KmeansCluster(object):
         """copy data into separate memory for clustering"""
         self._data = np.array(data)
 
-    def get_cluster(self, num_cluster, max_iter=10):
+    def get_cluster(self, num_cluster, max_iter=10, sort_range=None):
         """generate @num_cluster of clusters using @max_iter
         @para:
              num_cluster:   number of clusters
-             max_iter   :   maximum number of initials guess allowed"""
+             max_iter   :   maximum number of initials guess allowed,
+             sort_range :   the range of the vector elements sorted, ex (0,10)"""
         inertia = sys.float_info.max
         fin_cluster = []
+        if sort_range is None:
+            sort_range = (0, len(self._data[0]))  # use full vector
         for it in range(max_iter):
             centers = self._init_guess(num_cluster)
-            cluster = self._cluster_pts(centers)
+            cluster = self._cluster_pts(centers, sort_range)
             new_centers = self._recal_centers(cluster)
             while(not self._isconverged(centers, new_centers)):
                 centers = new_centers
-                cluster = self._cluster_pts(centers)
+                cluster = self._cluster_pts(centers, sort_range)
                 new_centers = self._recal_centers(cluster)
             # calculate inertia to evaluate the performance of this cluster
             new_inertia = self._calc_inertia(centers, cluster)
@@ -39,26 +42,28 @@ class KmeansCluster(object):
     def _calc_inertia(center, cluster):
         """calculate inertia/error to evaluate the performance of cluster"""
         return np.sum(np.sum([[np.linalg.norm(np.array(v) - np.array(center[i]))
-                                    for v in cluster[i]]
-                                    for i in range(len(center))]))
+                              for v in cluster[i]]
+                              for i in range(len(center))]))
 
     def _init_guess(self, num_cluster):
         """generate initial guess of the centers for each cluster"""
         sample_idx = [random.randint(0, len(self._data)-1) for i in range(num_cluster)]
         return self._data[sample_idx]
 
-    def _cluster_pts(self, centers):
+    def _cluster_pts(self, centers, sort_range):
         """Lloyd’s algorithm for performing k-means clustering"""
         # reference:
         # https://datasciencelab.wordpress.com/2013/12/12/clustering-with-k-means-in-python/
         cluster = {}
         for v in self._data:
-            bestcenterkey = min([(i[0], np.linalg.norm(v-centers[i[0]]))
-                         for i in enumerate(centers)], key=lambda t:t[1])[0]
+            full_v = v
+            v = v[sort_range[0]: sort_range[1]]  # slice the vector into desired range
+            bestcenterkey = min([(i[0], np.linalg.norm(v-centers[i[0]][sort_range[0]: sort_range[1]]))
+                                 for i in enumerate(centers)], key=lambda t:t[1])[0]
             try:
-                cluster[bestcenterkey].append(v)
+                cluster[bestcenterkey].append(full_v)
             except KeyError:
-                cluster[bestcenterkey] = [v]
+                cluster[bestcenterkey] = [full_v]
         return cluster
 
     @staticmethod
@@ -91,7 +96,7 @@ if __name__ == "__main__":
     # test cluster engine
     num_cluster = 3
     my_cluster = KmeansCluster(xy)
-    err,rst = my_cluster.get_cluster(num_cluster,max_iter=10)
+    err, rst = my_cluster.get_cluster(num_cluster, max_iter=10, sort_range=(0, 2))
 
     # plot results
     clr = ['red', 'green', 'blue', 'black']
